dbbe2bfb895861ced1e1917f7cc97da90544d4ec
Ville Vainio <vivainio@gmail.com>
2018-10-10 19:13:40 +0300

    publish.py

---
+++ publish.py
@@ -0,0 +1,26 @@
+from __future__ import print_function
+
+import os,shutil,glob
+
+prjdir = "TrivialBehinds"
+version = "1.1"
+def c(s):
+    print(">",s)
+    err = os.system(s)
+    assert not err
+
+def nuke(pth):
+    if os.path.isdir(pth):
+        shutil.rmtree(pth)
+
+def rm_globs(*globs):
+    for g in globs:
+        files = glob.glob(g)
+        for f in files:
+            print("Del",f)
+            os.remove(f)
+
+nuke(prjdir + "/bin")
+nuke(prjdir + "/obj")
+os.chdir(prjdir)
+c("dotnet pack /p:PackageVersion=%s -c Release" % version)
